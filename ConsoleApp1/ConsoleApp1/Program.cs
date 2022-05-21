cf44eab27c35f1e626cd47ffc138b7b0a52a1934
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-21 22:34:59 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            var input = " ";
+            int sum = 0;
+
+            while (input != "exit")
+            {
+                Console.WriteLine("Введите число, sum или exit");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "sum" :
+
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+
+                        Console.WriteLine($"Сумма: {sum}");
+                        sum = 0;
+                        break;
+                    case "exit":
+                        Console.WriteLine("Выход");
+                        break;
+                    default:
+                        int number = Convert.ToInt32(input);
+                        numbers.Add(number);
+                        break;
+                }
+            }
+            
+        }
+    }
+}
